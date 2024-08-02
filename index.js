cb6f915f020077c858bfc4fd432eabd95b72d005
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-08-02 14:09:54 +0200

    add styles sheet

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ let secondCard = 9
 
 let sum = firstCard + secondCard
 
+if (sum <= 20) {
+    console.log("Do you want to sraw a new card? 🙂")
+} else if (sum === 21 ) {
+    console.log ("Wohoo! You've got Blackjack! 🥳")
+} else 
+    console.log (" You're out if the game! 😭")
+
+
 console.log(sum)
+let 
