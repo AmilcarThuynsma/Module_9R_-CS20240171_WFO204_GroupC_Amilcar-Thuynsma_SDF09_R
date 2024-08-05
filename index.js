34a59a962ab56f25f47e31eedcb73fe840e961ba
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-08-05 23:23:36 +0200

    add for loop to game render function

--- index.js
+++ index.js
@@ -4,27 +4,46 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 10
-let secondCard = 4
+let secondCard = 10
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message =""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
 
 
-function startGame()  {
-    CardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+function renderGame()  {
+    cardsEl.textContent = "Cards: " 
+    for ( let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards [i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message ="Do you want to sraw a new card?"
+        message ="Do you want to draw a new card?"
     } else if (sum === 21 ) {
         message ="You've got Blackjack!"
         hasBlackJack=true
     } else {
-        message ="You're out if the game!"
+        message ="You're out of the game!"
         isAlive = false
     }    
     messageEl.textContent = message
+
+}
+
+function newCard() {
+    let card = 6
+    sum += card
+
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 
 }
