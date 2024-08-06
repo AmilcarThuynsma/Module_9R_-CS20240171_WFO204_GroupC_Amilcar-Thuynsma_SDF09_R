5b623db49ba1a29368c22f2a5a784750b9c2d67f
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-08-06 03:25:29 +0200

    add comments to index.js

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let player = { 
+    name: "Amilcar",
+    chips: 200
+}
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 10
-let secondCard = 10
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message =""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13) + 1
+    if (randomNumber > 10) {
+        return 10 
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
+
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard 
     renderGame()
 }
 
@@ -24,12 +42,13 @@ function renderGame()  {
     for ( let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards [i] + " "
     }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message ="Do you want to draw a new card?"
     } else if (sum === 21 ) {
         message ="You've got Blackjack!"
-        hasBlackJack=true
+        hasBlackJack= true
     } else {
         message ="You're out of the game!"
         isAlive = false
@@ -39,11 +58,10 @@ function renderGame()  {
 }
 
 function newCard() {
-    let card = 6
+   if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
     sum += card
-
     cards.push(card)
-    console.log(cards)
     renderGame()
-
+    }
 }
